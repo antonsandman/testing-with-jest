75b9b2015aa164c87e2212556dc019e0b2974aac
antonsandman <75663580+antonsandman@users.noreply.github.com>
2022-05-05 15:08:05 +0200

    lade till stack.js

---
+++ src/stack.js
@@ -0,0 +1,13 @@
+var _ = require('underscore');
+
+let stack = [];
+
+// Lägger ett element överst i stacken
+exports.push = function (x) {
+    stack.push(x);
+};
+
+// Returnerar det översta elementet i stacken och tar bort det
+exports.pop = function () {
+    return stack.pop();
+};
